e12fbca447183bba655de9f0e21e64eb1ac3c4ff
Alex Yatseyko <alex.yatseiko@gmail.com>
2020-07-01 09:26:21 +0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MainGameScreen/>
+    </View>
+  );
+}
+
+const MainGameScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.gameTitle}>Whack-a-mole!</Text>
+      <Image 
+        source={require('./assets/dirt.svg')}
+        style={styles.dirt}
+      />
     </View>
   );
 }
@@ -14,8 +25,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ffc600',
   },
+  gameTitle: {
+    textAlign: 'center',
+    fontSize: 60,
+    // lineHeight: 1
+    // margin-bottom: 0;
+  },
+  dirt: {
+    width: '50%',
+    height: 50,
+  }
 });
